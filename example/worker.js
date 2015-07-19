7b63b4acf30fde972f3ae08322ad608901545e61
Thomas Watson Steen <w@tson.dk>
2015-07-19 23:50:09 +0200

    Show isolation in example

--- example/worker.js
+++ example/worker.js
@@ -1,8 +1,13 @@
 var asyncState = require('async-state')
 
 process.on('uncaughtException', function (err) {
-  // output the URL for the HTTP request that caused the error
-  console.log('An error occurred for: ' + asyncState.req.url)
+  if (asyncState.req) {
+    // output the URL for the HTTP request that caused the error
+    console.log('An error occurred for: ' + asyncState.req.url)
+  } else {
+    console.log('An error occurred outside of an HTTP request')
+  }
+
   process.exit(1)
 })
 
