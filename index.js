130ad593fd457f88ec390b69cd13479525ee5eba
Thomas Watson Steen <w@tson.dk>
2015-07-19 23:51:02 +0200

    Improve code comments for undocumented API

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ function AsyncState () {
   })
 
   function asyncFunctionInitialized () {
+    // Record the state currently set on on the async-state object and return a
+    // snapshot of it. The returned object will later be passed as the `data`
+    // arg in the functions below.
+
     var data = {}
     for (var key in state) {
       data[key] = state[key]
@@ -28,12 +32,17 @@ function AsyncState () {
   }
 
   function asyncCallbackBefore (context, data) {
+    // We just returned from the event-loop: We'll now restore the state
+    // previously saved by `asyncFunctionInitialized`.
+
     for (var key in data) {
       state[key] = data[key]
     }
   }
 
   function asyncCallbackAfter (context, data) {
+    // Clear the state so that it doesn't leak between isolated async stacks.
+
     for (var key in state) {
       delete state[key]
     }
