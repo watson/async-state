6279bf13a587edcc0ff782e2448b0e8ed04d9529
Thomas Watson Steen <w@tson.dk>
2015-09-18 14:44:27 +0200

    Revert "Deep clone objects on async boundaries"
    This reverts commit 279d3e39e623c2049e3e87ca7a97001ee05dec1c.

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 
 require('async-listener')
 
-var deepExtend = require('deep-extend')
-
 module.exports = new AsyncState()
 
 function AsyncState () {
@@ -21,14 +19,20 @@ function AsyncState () {
     // snapshot of it. The returned object will later be passed as the `data`
     // arg in the functions below.
 
-    return deepExtend({}, state)
+    var data = {}
+    for (var key in state) {
+      data[key] = state[key]
+    }
+    return data
   }
 
   function asyncCallbackBefore (context, data) {
     // We just returned from the event-loop: We'll now restore the state
     // previously saved by `asyncFunctionInitialized`.
 
-    deepExtend(state, data)
+    for (var key in data) {
+      state[key] = data[key]
+    }
   }
 
   function asyncCallbackAfter (context, data) {
